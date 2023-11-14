c67cab3951fdfd376c097723ec17407b43d62ef4
globuusss <149681009+globuusss@users.noreply.github.com>
2023-11-14 20:13:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function myFunction() {
+  var paq = Math.floor(Math.random() * val)
+  console.log(paq);
+  if (paq == 1) {
+    document.getElementById("demo").innerHTML="Ситора опа"
+  }else if(paq == 2){
+    document.getElementById("demo").innerHTML="Марго"
+  }else if(paq == 3){
+    document.getElementById("demo").innerHTML="Роботекс"
+  }else if(paq == 4){
+    document.getElementById("demo").innerHTML="Гулизар"
+  }else if(paq == 5){
+    document.getElementById("demo").innerHTML="Лайло"
+  }else if(paq == 6){
+    document.getElementById("demo").innerHTML="ИРОДА АПХЗАХЗПА ГОТОВЬ ЧТО ИЗ ТВОИХ УХ БУДЕТ ТЕЧЬ КРОВЬ  АХЗАЗАХАХАХАХ"
+  }else if(paq == 7){
+    document.getElementById("demo").innerHTML="Ясмин"
+  }else if(paq == 8){
+    document.getElementById("demo").innerHTML="Шахзода"
+  }else if(paq == 9){
+    document.getElementById("demo").innerHTML="Иветтка"
+  }else if(paq == 10){
+    document.getElementById("demo").innerHTML="Гаяна"
+  }else if(paq == 11){
+    document.getElementById("demo").innerHTML="Диера"
+  }else if(paq == 12){
+    document.getElementById("demo").innerHTML="ЗЕБИНИСО"
+  }else if(paq == 13){
+    document.getElementById("demo").innerHTML="Валера нефор ока"
+  }
+  }
+let val = 14
+
+// document.getElementById("demo").innerHTML=""
+// var paq = Math.floor(Math.random() * val)
+// console.log(paq);
